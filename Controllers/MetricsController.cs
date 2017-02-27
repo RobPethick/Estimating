dc7c7e75aab6aa58ba5340590552f5c767863399
Rob - Kate <Rob - Kate>
2017-02-27 20:11:47 +0000

    Commit basic webapi project

---
+++ Controllers/MetricsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Code.Controllers
+{
+    [Route("api/[controller]")]
+    public class MetricsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Metric> Get()
+        {
+            return new Metric[] { 
+                new Metric(){Description = "Analysis", Default = 20, Order = -1}, 
+                new Metric(){Description = "Testing", Default = 50, Order = 1}};
+        }
+    }
+}
---
+++ Models/Metric.cs
@@ -0,0 +1,5 @@
+public class Metric{
+    public double Default{ get; set; }
+    public string Description { get; set; }
+    public int Order { get; set;}
+}
