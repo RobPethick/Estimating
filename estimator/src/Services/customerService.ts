5525515e57e2bf2dc5573da56a7eadbe04f6b892
Rob Pethick <robert.pethick@softwaresolved.com>
2017-05-18 15:34:48 +0100

    use abbreviation

--- estimator/src/Services/customerService.ts
+++ estimator/src/Services/customerService.ts
@@ -5,7 +5,7 @@ export class CustomerService{
     public getCustomers(){
         var rateA = new RateModel("DT", "Dev/Test", 100);
         var rateB = new RateModel("TC", "Technical Consultant", 200);
-        var rateC = new RateModel("LTC", "Lead Technical Consultant", 250);
+        var rateC = new RateModel("LTC", "Lead TC", 250);
         var rateD = new RateModel("PM", "Project Manager", 200);
         return [new CustomerModel("CustomerA", [rateA, rateB, rateC, rateD]), new CustomerModel("CustomerB", [rateA, rateB, rateC, rateD])];
     }
