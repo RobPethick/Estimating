c917c3e475cab89e74b541f19e6c17cce1af54c5
Rob Pethick <robert.pethick@softwaresolved.com>
2017-05-25 17:40:01 +0100

    Removing double title

--- estimator/src/app.ts
+++ estimator/src/app.ts
@@ -4,7 +4,6 @@ export class App {
   public router: Router;
 
   public configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Estimator';
     config.map([
       { route: ['', 'estimator'], name: 'estimator', moduleId: 'estimator', nav: true, title: 'Estimator' }
     ]);
