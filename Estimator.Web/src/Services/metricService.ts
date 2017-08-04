49ffb5d1c39b4400d505a9cac1aea1449787cc0e
Rob - Kate <robert.pethick1@gmail.com>
2017-08-04 21:21:27 +0100

    Continue switch to async from Promise

--- Estimator.Web/src/Services/metricService.ts
+++ Estimator.Web/src/Services/metricService.ts
@@ -4,26 +4,24 @@ import { MetricDefaultsModel } from "../Models/metricDefaultsModel";
 import { HttpClient } from "aurelia-fetch-client";
 
 export class MetricService {
-    public getDefaultMetrics(): Promise<Array<MetricDefaultsModel>> {
+    public async getDefaultMetrics(): Promise<Array<MetricDefaultsModel>> {
         let httpClient = new HttpClient();
         httpClient.configure(config => {
             config.useStandardConfiguration()
                 .withBaseUrl('api/');
         });
 
-        return httpClient.fetch('metrics', { method: 'get' })
-            .then(response => response.json())
-            .then(responseJson => {
-                let metricDefaults = new Array<MetricDefaultsModel>();
-                responseJson as Array<any>;
-                responseJson.forEach(metricDefaultJson => {
-                    let metricDefault = new MetricDefaultsModel(metricDefaultJson.name, []);
-                    metricDefaultJson.metrics.forEach(metric => {
-                        metricDefault.metrics.push(new MetricModel(metric.name, metric.defaultPercentage, RateTypeModel.GetRateTypeModelFromCode(metric.rateCode)))
-                    });
-                    metricDefaults.push(metricDefault);
-                });
-                return metricDefaults;
+        var response = await httpClient.fetch('metrics', { method: 'get' });
+        var responseJson = await response.json();
+        let metricDefaults = new Array<MetricDefaultsModel>();
+        responseJson as Array<any>;
+        responseJson.forEach(metricDefaultJson => {
+            let metricDefault = new MetricDefaultsModel(metricDefaultJson.name, []);
+            metricDefaultJson.metrics.forEach(metric => {
+                metricDefault.metrics.push(new MetricModel(metric.name, metric.defaultPercentage, RateTypeModel.GetRateTypeModelFromCode(metric.rateCode)))
             });
+            metricDefaults.push(metricDefault);
+        });
+        return metricDefaults;
     }
 }
